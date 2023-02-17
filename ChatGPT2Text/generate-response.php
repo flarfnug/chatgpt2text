77f19bab35180e0174ea4ff8b475d83aacc3d898
Flarfnug <108523323+flarfnug@users.noreply.github.com>
2023-02-16 19:33:10 -0500

    filtering/sanity added
    added some security measures including access controls by IP and defining your API key outside of any active calls

--- ChatGPT2Text/generate-response.php
+++ ChatGPT2Text/generate-response.php
@@ -1,12 +1,28 @@
 <?php
 
-// Check to see if a prompt file exists, if no create one, if no text is entered use a default prompt
-$prompt = isset($_POST['prompt']) ? $_POST['prompt'] : '';
+// Define your API key
+define('OPENAI_API_KEY', 'pleasetypeyourAPIkeyorthisallwontwork');
+
+// Sanitize input
+$prompt = isset($_POST['prompt']) ? filter_input(INPUT_POST, 'prompt', FILTER_SANITIZE_STRING) : '';
 if (empty($prompt)) {
   $prompt = file_exists('prompt.txt') ? file_get_contents('prompt.txt') : 'Write a funny, witty, unique, and intelligent quote.';
 } else {
   // Save the prompt to a file
   file_put_contents('prompt.txt', $prompt);
+}
+
+// Validate the API key (replace $OPENAI_API_KEY$ with your actual API key)
+if (!defined('OPENAI_API_KEY')) {
+  define('OPENAI_API_KEY', '$OPENAI_API_KEY$');
+}
+
+// Set access controls
+$allowed_ips = ['127.0.0.1']; // Example: allow only localhost
+$remote_ip = $_SERVER['REMOTE_ADDR'];
+if (!in_array($remote_ip, $allowed_ips)) {
+  http_response_code(403);
+  exit('Access denied');
 }
 
 // Array to configure the model
@@ -28,15 +44,23 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
   'Content-Type: application/json',
-  'Authorization: Bearer $OPENAI_API_KEY$'
+  'Authorization: Bearer ' . OPENAI_API_KEY
 ));
 
 // Execute the curl request
 $response = curl_exec($ch);
+if (curl_errno($ch)) {
+  http_response_code(500);
+  exit('Error calling OpenAI API: ' . curl_error($ch));
+}
 curl_close($ch);
 
-// Get the generated text from the response
+// Validate response
 $json = json_decode($response, true);
+if (!$json || !isset($json['choices']) || !isset($json['choices'][0]) || !isset($json['choices'][0]['text'])) {
+  http_response_code(500);
+  exit('Invalid response from OpenAI API');
+}
 $text = $json['choices'][0]['text'];
 
 // Save the generated text to a file
